3dcda63056888f20a83a128ae032ea3e5b0db9ca
blurryroots <blurryroots@posteo.de>
2015-03-27 15:06:02 +0100

    rewrote 'left' logic

--- ubuntu-trusty/left.sh
+++ ubuntu-trusty/left.sh
@@ -5,29 +5,64 @@ _battery_left_usage () {
 	echo "usage: battery left [options]"
 }
 
+# calculate something ($2) with a certain precision ($1)
+batter_left_calc () {
+	local precision="scale = $1"
+	local formular="$2"
+	local bc_arg="$precision; $formular"
+
+	echo $(echo "$bc_arg" | bc -l)
+}
+
+#
+batter_left_percent () {
+	local charge_now=`cat $_battery_path/charge_now`
+	local charge_full=`cat $_battery_path/charge_full`
+
+	local formular="($charge_now / $charge_full) * 100"
+	echo $(batter_left_calc 2 "$formular")%
+}
+
+#
+batter_left_time () {
+	local charge_full=`cat $_battery_path/charge_full`
+	local current_now=`cat $_battery_path/current_now`
+
+	local formular="($charge_full / $current_now)"
+	local hours_left=$(batter_left_calc 2 "$formular")
+	local hours_only=$(batter_left_calc 0 "$hours_left / 1")
+	local minutes_only=$(batter_left_calc 2 "($hours_left - $hours_only) * 60")
+
+	echo "$hours_only h, $minutes_only m"
+}
+
 #
 battery_left () {
-	local batdir="$_battery_path/uevent"
-	local rx="POWER_SUPPLY_CHARGE_NOW=([0-9]+)"
+	local r
 
-	local cfull
-	local cnow
+	[ $# -eq 0 ] && {
+		echo "$(batter_left_time) ($(batter_left_percent))"
+	} || {
+		while getopts hpt option; do
+			case "$option" in
+				h) {
+					_battery_left_usage
+				} ;;
 
-	#
-	cnow=$(cat $batdir | {
-		# -n not print all lines -r activate regex
-		# s/(searching) /(matching) /p(print) 
-		sed -nr "s/POWER_SUPPLY_CHARGE_NOW=([0-9]+)/\1/p"
-	})
+				p) {
+					batter_left_percent
+				} ;;
 
-	#
-	cfull=$(cat $batdir | {
-		# -n not print all lines -r activate regex
-		# s/(searching) /(matching) /p(print) 
-		sed -nr "s/POWER_SUPPLY_CHARGE_FULL=([0-9]+)/\1/p"
-	})
+				t) {
+					batter_left_time
+				} ;;
+			esac
 
-	echo $(echo "scale = 2; ($cnow/$cfull) * 100" | bc)%
+			r=$?
+		done
+	}
+
+	exit $r
 }
 
 battery_left $@
